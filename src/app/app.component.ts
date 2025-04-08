fe96d9be1f26544c44ca3288f7e5a86c7188501e
LanaNew <l4n.steen@gmail.com>
2025-04-08 17:56:59 +0400

    ang 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,5 +10,54 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ang1';
+  
+   greeting = 'Hello World!'
+
+   age = 30
+
+
+   text? : string | null
+   
+    // nullable
+
+    text2! : string
+
+   // ! გამოყენებისას მივაწვდით ინფომრაციას
+
+
+    user = {
+       name : "John",
+       lastName : "Doe",
+       age : 50       
+    }
+
+    isActive : boolean = true
+
+
+    numArr : (string | number)[] =["jgfgf", 11]
+    imgLink = "https://images.unsplash.com/photo-1742590794223-5e55f1c9e63f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"
+    mainColor = ""
+
+
+
+    printHello(){
+      console.log("hi")
+      this.mainColor="mainColor"
+    }
+
+
+
+    forChangindColor = ""
+
+
+    chnageCOlor(){
+      this.forChangindColor=="" ? this.forChangindColor ="forColor" : this.forChangindColor =""
+    }
+ 
 }
+
+
+
+
+
+
