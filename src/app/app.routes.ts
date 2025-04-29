1986def507cd1c70c4f8423d9508fa709f5c6f3b
LanaNew <l4n.steen@gmail.com>
2025-04-29 18:14:48 +0400

    ang 7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { StudentsComponent } from './students/students.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "students", component: StudentsComponent},
     {path: "user", component:UserComponent},
     {path: "details/:id", component:UserDetailsComponent},
+    {path: "products", component: ProductsComponent},
+    {path: "products/:id", component: ProductDetailsComponent}
 ];
